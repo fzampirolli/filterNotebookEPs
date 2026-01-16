bab6a2504d35e7e57dccf5398f83ad94309a074f
Francisco de Assis Zampirolli <fzampirolli@gmail.com>
2026-01-16 10:33:43 -0300

    v1

--- cases_gab/cap2/EP2_2.py
+++
@@ -1,9 +0,0 @@
-# sua solução
-# incluir no VPL APENA a função;
-def somar(a, b):
-    # Implemente a lógica aqui e retorne o valor
-    return a+b
-
-# não incluir o que segue:
-r=somar(int(input()), int(input()))
-print(r)
